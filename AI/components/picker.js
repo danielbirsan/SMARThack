43fd3ae67ac3bdaf05e2baf5f3fe541e608db6e5
danielbirsan <75643437+danielbirsan@users.noreply.github.com>
2023-11-12 01:31:50 +0200

    working backend

--- AI/components/picker.js
+++ AI/components/picker.js
@@ -1,84 +1,101 @@
 import React, { useState } from "react";
-import {
-    View, Text, Image, TouchableOpacity,
-    StyleSheet, Alert
-} from "react-native";
+import { View, Text, Image, TouchableOpacity, StyleSheet, Alert } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 export default function ImageUpload() {
-
-    // Stores the selected image URI 
     const [file, setFile] = useState(null);
 
-    // Stores any error message 
-    const [error, setError] = useState(null);
-
-    // Function to pick an image from  
-    //the device's media library 
     const pickImage = async () => {
-        const { status } = await ImagePicker.
-            requestMediaLibraryPermissionsAsync();
+        try {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            });
 
-        if (status !== "granted") {
+            if (!result.canceled) {
+                setFile(result.assets[0].uri);
 
-            // If permission is denied, show an alert 
-            Alert.alert(
-                "Permission Denied",
-                `Sorry, we need camera  
-                 roll permission to upload images.`
-            );
-        } else {
-
-            // Launch the image library and get 
-            // the selected image 
-            const result =
-                await ImagePicker.launchImageLibraryAsync();
-
-            if (!result.cancelled) {
-
-                // If an image is selected (not cancelled),  
-                // update the file state variable 
-                setFile(result.uri);
-
-
-                // Clear any previous errors 
-                setError(null);
+                // Send the selected image to the server
+                console.log(result.log)
+                sendImageToServer(result.assets[0].uri);
             }
+        } catch (error) {
+            console.error("Error:", error);
+            Alert.alert("Error", "Failed to pick an image.");
         }
+    };
 
+    const sendImageToServer = async (imageUri) => {
+        try {
+            const apiUrl = "https://ab97-81-196-9-50.ngrok.io/image"; // Replace with your server endpoint
+
+            const formData = new FormData();
+            formData.append("photo", {
+                uri: imageUri,
+                type: "image/jpeg",
+                name: "photo.jpg",
+            });
+
+            const response = await fetch(apiUrl, {
+                method: "POST",
+                body: formData
+            });
+
+            if (response.ok) {
+                // Handle the success response from the server
+                console.log("Image uploaded successfully!");
+            } else {
+                // Handle server error or other issues
+                console.error("Failed to upload image to the server.");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            Alert.alert("Error", "Failed to send image to the server.");
+        }
     };
 
     return (
         <View style={styles.container}>
-            <Text style={styles.header}>
-                Add Image:
-            </Text>
+            <Text style={styles.header}>Add Image:</Text>
 
-            {/* Button to choose an image */}
-            <TouchableOpacity style={styles.button}
-                onPress={pickImage}>
-
-                <Text style={styles.buttonText}>
-                    Choose Image
-                </Text>
+            {/* Button to choose image */}
+            <TouchableOpacity style={styles.button} onPress={pickImage}>
+                <Text style={styles.buttonText}>Choose Image</Text>
             </TouchableOpacity>
 
-            {/* Conditionally render the image  
-            or error message */}
+            {/* Conditionally render the image or error message */}
             {file ? (
-                // Display the selected image 
                 <View style={styles.imageContainer}>
-                    <Image source={{ uri: file }}
-                        style={styles.image} />
+                    <Image source={{ uri: file }} style={styles.image} />
                 </View>
             ) : (
-                // Display an error message if there's  
-                // an error or no image selected 
-                <Text style={styles.errorText}>{error}</Text>
+                <Text style={styles.errorText}></Text>
             )}
         </View>
     );
 }
+
+// return (
+//     <View style={styles.container}>
+//         <Text style={styles.header}>Add Images:</Text>
+
+//         {/* Button to choose images */}
+//         <TouchableOpacity style={styles.button} onPress={pickImage}>
+//             <Text style={styles.buttonText}>Choose Images</Text>
+//         </TouchableOpacity>
+
+//         {/* Conditionally render the images or error message */}
+//         {files.length > 0 ? (
+//             <View style={styles.imageContainer}>
+//                 {files.map((file, index) => (
+//                     <Image key={index} source={{ uri: file.uri }} style={styles.image} />
+//                 ))}
+//             </View>
+//         ) : (
+//             <Text style={styles.errorText}></Text>
+//         )}
+//     </View>
+// );
+// }
 
 const styles = StyleSheet.create({
     container: {
@@ -108,18 +125,17 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
     },
     imageContainer: {
-        borderRadius: 8,
+        flexDirection: "row",
+
+        justifyContent: "center",
         marginBottom: 16,
-        shadowColor: "#000000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.4,
-        shadowRadius: 4,
-        elevation: 5,
     },
     image: {
-        width: 200,
+        width: 100,
         height: 200,
+        resizeMode: "contain",
         borderRadius: 8,
+        margin: 8,
     },
     errorText: {
         color: "red",
