d6de2418f6cc8a2a3c14958650a09d8269646073
danielbirsan <75643437+danielbirsan@users.noreply.github.com>
2023-11-12 08:34:22 +0200

    ok

--- backend/app.py
+++ backend/app.py
@@ -12,11 +12,28 @@ cors = CORS(app)
 
 @app.route("/image", methods=['GET', 'POST'])
 def image():
-    if(request.method == "POST"):
-        bytesOfImage = request.get_data()
-        file = './input/image.jpeg'
-        # file = './image/example.jpg'
-        with open(file, 'wb') as out:
-            out.write(bytesOfImage)
-        res = getJSONReceit(file)
-        return res
+    if (request.method == "POST"):
+        # bytesOfImage = request.get_data()
+        # file = './image/image.jpg'
+        # # file = './image/example.jpg'
+        # with open(file, 'wb') as out:
+        #     out.write(bytesOfImage)
+        # print("Image received", file)
+        # res = getJSONReceit(file)
+
+        # if 'photo' not in request.files:
+        # return jsonify({'error': 'No photo provided'}), 400
+
+        photo = request.files['photo']
+
+        if photo.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+
+        # Save the photo to a specific folder (replace 'uploads' with your folder name)
+        path = 'C:/Users/Daniel/Desktop/smarthack/SMARThack/backend/image/' + photo.filename
+
+        print("Image received", path)
+        photo.save(path)
+        res = getJSONReceit(path)
+
+        return jsonify(res), 200
